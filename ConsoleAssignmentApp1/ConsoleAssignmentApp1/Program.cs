7b92de3a8813e42331d7c3b00a6aeac885b4d9b7
unknown <richbashar@gmail.com>
2019-09-11 00:14:46 +0600

    Console Assigment App1

---
+++ ConsoleAssignmentApp1/ConsoleAssignmentApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAssignmentApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "";
+            int Phone_Number;
+            int Age;
+            string Address = "";
+            
+            Console.WriteLine("Enter Your Name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Your Phone Number:");
+            Phone_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Age: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Address:");
+            Address = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Show My Details");
+            Console.WriteLine("MY Name Is : " + Name);
+            Console.WriteLine("My Phone Number : " + Phone_Number);
+            Console.WriteLine("My Age : " + Age);
+            Console.WriteLine("My Address is: " + Address);
+
+            Console.ReadLine();
+        }
+    }
+}
